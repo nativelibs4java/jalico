6fa458c7d6bcead95c0350d492c5f70abcd12d74
olivier.chafik <olivier.chafik@25527fbf-9e46-0410-83e2-a97ad06868d3>
2008-06-28 11:07:37 +0000

    Added SyntaxUtils utilitary class

---
+++ sources/com/ochafik/lang/SyntaxUtils.java
@@ -0,0 +1,28 @@
+package com.ochafik.lang;
+
+import javax.swing.tree.TreeNode;
+
+import com.ochafik.util.grammar.SwingParseTreeNode;
+
+public class SyntaxUtils {
+	public static <T> T[] array(T... elements) {
+		return elements;
+	}
+	
+	public static <T> boolean equal(T a, T b) {
+		if (a == null)
+			return b == null;
+		if (b == null)
+			return false;
+		return a.equals(b);
+	}
+
+	public static <T> T as(Object node, Class<T> c) {
+		if (node == null)
+			return null;
+		if (c.isAssignableFrom(node.getClass()))
+			return c.cast(node);
+		return null;
+	}
+	
+}
